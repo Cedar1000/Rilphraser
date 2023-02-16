2de88912892005629f5de5dd01028a66a77bbf62
Cedar Daniel <cedardaniel1998@gmail.com>
2023-02-17 00:50:18 +0100

    fixed scraping and paraphrase

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi, createEdit } = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
 
 const dotenv = require('dotenv');
 dotenv.config({ path: 'config.env' });
@@ -8,6 +8,7 @@ const cors = require('cors');
 
 const cheerio = require('cheerio');
 const axios = require('axios');
+const puppeteer = require('puppeteer');
 
 const app = express();
 
@@ -15,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 
 const configuration = new Configuration({
-  organization: 'org-pR5f7p7el93EFBbWk8OJ8QOa',
   apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -26,10 +26,13 @@ const url = 'https://renaissancelabs.org/';
 app.post('/scrape-only', async (req, res) => {
   const { url } = req.body;
   try {
-    const response = await axios.get(url);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-    const $ = cheerio.load(response.data);
-    const text = $('p').text();
+    const text = await page.evaluate(() => document.body.innerText);
+
+    await browser.close();
 
     res.status(200).json({
       status: 'success',
@@ -48,23 +51,61 @@ app.post('/scrape-and-rewrite', async (req, res) => {
   const { url } = req.body;
 
   try {
-    const html = await axios.get(url);
+    // const html = await axios.get(url);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-    const $ = cheerio.load(html.data);
-    const input = $('p').text();
-    console.log(input);
+    const text = await page.evaluate(() => document.body.innerText);
 
-    const response = await openai.createEdit({
-      model: 'code-davinci-edit-001',
-      input,
-      instruction: 'rewrite the sentence in another way',
+    await browser.close();
+    // const $ = cheerio.load(html.data);
+    const inputArray = text.split('.');
+    const middleIndex = Math.round(inputArray.length / 2);
+
+    const firstPart = inputArray.slice(0, middleIndex).join('. ');
+    const secondPart = inputArray.slice(middleIndex).join('. ');
+
+    // const response = await openai.createEdit({
+    //   model: 'code-davinci-edit-001',
+    //   input: text.replace(/[\r\n]/gm, ''),
+    //   instruction: 'rewrite this differently',
+    // });
+
+    const options = (text) => ({
+      method: 'POST',
+      url: 'https://api.textcortex.com/v1/texts/paraphrases',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.TEXT_CORTEX_API_KEY}`,
+      },
+      data: {
+        keywords: ['string'],
+        max_tokens: 500,
+        n: 1,
+        source_lang: 'en',
+        target_lang: 'en',
+        temperature: 0.8,
+        text,
+      },
     });
 
-    const [result] = response.data.choices;
+    const response1 = await axios.request(options(firstPart));
+
+    const [data1] = response1.data.data.outputs;
+
+    const response2 = await axios.request(options(secondPart));
+
+    const [data2] = response2.data.data.outputs;
+
+    // console.log(response.data.choices);
+
+    // const [result] = response.data.choices;
 
     res.status(200).json({
       status: 'success',
-      data: result,
+      // data: result.text,
+      data: `${data1.text} ${data2.text}`,
     });
   } catch (error) {
     console.error(error);
@@ -80,17 +121,52 @@ app.post('/rewrite-only', async (req, res) => {
   const { input } = req.body;
 
   try {
-    const response = await openai.createEdit({
-      model: 'code-davinci-edit-001',
-      input,
-      instruction: 'rewrite the sentence in another way',
+    const inputArray = input.split('.');
+    const middleIndex = Math.round(inputArray.length / 2);
+
+    const firstPart = inputArray.slice(0, middleIndex).join('. ');
+    const secondPart = inputArray.slice(middleIndex).join('. ');
+
+    // const response = await openai.createEdit({
+    //   model: 'code-davinci-edit-001',
+    //   input: text.replace(/[\r\n]/gm, ''),
+    //   instruction: 'rewrite this differently',
+    // });
+
+    const options = (text) => ({
+      method: 'POST',
+      url: 'https://api.textcortex.com/v1/texts/paraphrases',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.TEXT_CORTEX_API_KEY}`,
+      },
+      data: {
+        keywords: ['string'],
+        max_tokens: 500,
+        n: 1,
+        source_lang: 'en',
+        target_lang: 'en',
+        temperature: 0.8,
+        text,
+      },
     });
 
-    const [result] = response.data.choices;
+    const response1 = await axios.request(options(firstPart));
+
+    const [data1] = response1.data.data.outputs;
+
+    const response2 = await axios.request(options(secondPart));
+
+    const [data2] = response2.data.data.outputs;
+
+    // console.log(response.data.choices);
+
+    // const [result] = response.data.choices;
 
     res.status(200).json({
       status: 'success',
-      data: result.text.replace(/[\r\n]/gm, ''),
+      // data: result.text,
+      data: `${data1.text} ${data2.text}`,
     });
   } catch (error) {
     console.error(error);
